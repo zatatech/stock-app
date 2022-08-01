35bb805722886e5b38ffe475fbbd847a3454a682
zatatech <zatatech@gmail.com>
2022-08-01 09:14:03 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+      console.log("pet.recibida")
+      res.send('<h1>hola mundo<h1>')
+})
+
+app.listen(4000,()=>{
+    console.log("servidor escuchando en el puertro 4000")
+})
